7eac8368031f5732d7d0f45296d5004dfdccc9cc
Tymoteusz Adamczyk <tymoteusz.adamczyk@outlook.com>
2017-03-07 15:28:34 +0100

    06.02

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base for any basic moving character. Requires additional component
+/// like PlayerPlatformController to actually do anything.
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlatformController : MonoBehaviour
+{
+    // Public variables are shown in Unity's inspector and can be edited through it at any time
+    [Tooltip("Max horizontal movement speed")]
+    public float moveSpeed = 5f;
+
+    // Same goes for private variables with SerializeField tag.
+    [Tooltip("Force applied in y+ direction at jump")]
+    [SerializeField]
+    private int jumpForce = 300;
+
+    // Rigibody tells Unity to enable physics on this game object.
+    private Rigidbody2D localRigibody;
+
+    void Start()
+    {
+        localRigibody = GetComponent<Rigidbody2D>();
+    }
+
+    /// <summary>
+    /// Sets acceleration of a character by moveValue * moveSpeed and can make it jump.
+    /// Gravity, collisions etc. are handled by Unity's physics engine (Box2D).
+    /// </summary>
+    /// <param name="moveValue">Multiplier of max speed</param>
+    /// <param name="wantToJump">True = jump</param>
+    public void Move(float moveValue, bool wantToJump)
+    {
+        // Jump
+        if (wantToJump)
+        {
+            localRigibody.AddForce(new Vector2(0, jumpForce));
+        }
+
+        // Horizontal movement.
+        float newHorizontalVelocity = moveValue * moveSpeed;
+        Vector2 newVelocity = new Vector2(newHorizontalVelocity, localRigibody.velocity.y);
+        localRigibody.velocity = newVelocity;
+    }
+}
---
+++ Assets/Scripts/PlayerPlatformController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Player controlls for PlatformController.
+/// </summary>
+// Unity will make sure that PlatformController will be attached to this game object.
+[RequireComponent(typeof(PlatformController))]
+public class PlayerPlatformController : MonoBehaviour
+{
+    private PlatformController thisController;
+    private bool hasJumped = false;
+
+    // Use this for initialization (executes at object spawn).
+    void Start()
+    {
+        thisController = GetComponent<PlatformController>();
+    }
+
+    // Update executes at every GRAPHICAL frame.
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            hasJumped = true;
+        }
+    }
+
+    // FixedUpdate executes at every PHYSICAL frame.
+    void FixedUpdate()
+    {
+        // Axis are defined in Unity's editor: Edit -> Project Settings -> Input
+        float x = Input.GetAxis("Horizontal");
+
+        // We're moving character every physical frame because we're using Unity physics.
+        // Always remember to deal with physics in FixedUpdate.
+        thisController.Move(x, hasJumped);
+        hasJumped = false;
+    }
+}
